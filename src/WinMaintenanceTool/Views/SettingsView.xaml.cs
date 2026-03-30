49f6daeb834ded485549f731d4136c567d994531
John Wiliam <djhonathan.wilian@gmail.com>
2026-03-29 20:21:00 -0400

    Create SettingsView.xaml.cs

---
+++ src/WinMaintenanceTool/Views/SettingsView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WinMaintenanceTool.Views;
+
+public partial class SettingsView : UserControl
+{
+    public SettingsView()
+    {
+        InitializeComponent();
+    }
+}
